b6360135f9d7826c0283cf054331d664232d8222
Fitzix <caojunkaiv@gmail.com>
2018-11-28 20:51:19 +0800

    add send example

--- bi.go
+++ bi.go
@@ -21,7 +21,7 @@ type BIModel interface {
 func NewGameleySender(addr string, gameId int) *GameleySender {
 	udpAddr, err := net.ResolveUDPAddr("udp4", addr)
 	if err != nil {
-		log.Print("配置Addr有误")
+		log.Fatal("【BiSDK】", "配置Addr有误")
 		return nil
 	}
 	udpConn, err := net.DialUDP("udp4", nil, udpAddr)
@@ -34,8 +34,12 @@ func NewGameleySender(addr string, gameId int) *GameleySender {
 
 func (g *GameleySender) Send(biModel BIModel) {
 	if _, err := g.conn.Write([]byte(biModel.ToString(strconv.Itoa(g.GameId)))); err != nil {
-		print("发送日志 ----> ", biModel.ToString(strconv.Itoa(g.GameId)), " 失败 ")
+		log.Print("【BiSDK】", "发送日志失败", err)
 		return
 	}
-	log.Print("发送日志 ---> ", biModel.ToString(strconv.Itoa(g.GameId)), " 成功 ")
+	log.Print("【BiSDK】", "发送日志 ---> ", biModel.ToString(strconv.Itoa(g.GameId)), " 成功 ")
+}
+
+func (g *GameleySender) Close() error {
+	return g.conn.Close()
 }
--- examples/send.go
+++ examples/send.go
@@ -11,8 +11,15 @@ import (
 
 func main() {
 	client := bigo.NewGameleySender(":8888", 12)
-	logPlat := bimodels.NewLogPlat("1213313sdasd",1233123, time.Now())
-	logRole := bimodels.NewLogRole(logPlat, time.Now(), "2323", 123123, 12, 1, 1, 1)
-	logAdVideo := bimodels.NewLogAdVideo(logRole, 5, bigo.LogEnumAdEnd)
-	client.Send(logAdVideo)
+	logPlat := bimodels.NewLogPlat("1213313sdasd",2006603, time.Now())
+	//logDevices := bimodels.NewLogDevices("sdasd322323", "1.1.1.", "sdad", "asda", "asdasda", 1, 1, "sadad","asdads",233,232,32,"sdsd","sdads","31312313")
+	logDevices := &bimodels.LogDevices{}
+	logRole := bimodels.NewLogRole(logPlat, time.Now(), "2323", 1010001, 12, 1, 1, 1)
+	//logReason := bimodels.NewLogReason(logRole, "1212", "委屈翁群二", "0")
+	//logAdVideo := bimodels.NewLogAdVideo(logRole, 5, bigo.LogEnumAdEnd)
+	logReg := bimodels.NewLogReg(logRole, logDevices)
+	//logEquip := bimodels.NewLogEquip(logReason, 1, 1, 1, 1)
+	//client.Send(logAdVideo)
+	client.Send(logReg)
+	//client.Send(logEquip)
 }
